6032a1a922b31f3f99d5c0e29e5e37637af0f392
weshaw <weshaw@gmail.com>
2018-06-22 16:00:11 -0500

    add a few usefull prototypes Array.unique dom.addClass dom.removeClass

---
+++ polyfills.js
@@ -0,0 +1,17 @@
+Array.prototype.unique = function()
+{
+	return this.filter(function(el, index, arr){ return index == arr.indexOf(el); });
+};
+
+HTMLElement.prototype.addClass = function(c)
+{
+	this.className = this.className.split(" ").concat(c.split(" ")).unique().filter(Boolean).join(" ");
+	return this;
+};
+
+HTMLElement.prototype.removeClass = function(c)
+{
+	c = c.split(" ");
+	this.className = this.className.split(" ").filter(function(e){ return c.indexOf(e) < 0; }).join(" ");
+	return this;
+};
